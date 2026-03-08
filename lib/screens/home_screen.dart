63601941a68548edf681756be95d35428605da7a
Sudarshan <godlsudarsan@gmail.com>
2026-03-08 11:09:52 +0530

    made changes in the ui to add tasks

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'create_rule_screen.dart';
+import '../engine/rule_manager.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final rules = RuleManager.getRules();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Automation Rules"),
       ),
-      body: const Center(
-        child: Text(
-          "No rules yet",
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
+
+      body: rules.isEmpty
+          ? const Center(
+              child: Text(
+                "No rules yet",
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView.builder(
+              itemCount: rules.length,
+              itemBuilder: (context, index) {
+
+                final rule = rules[index];
+
+                return ListTile(
+                  title: Text(rule.name),
+                  subtitle: Text("Trigger: ${rule.triggerType}"),
+                );
+              },
+            ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const CreateRuleScreen(),
+            ),
+          );
+
+          (context as Element).markNeedsBuild();
+        },
+
         child: const Icon(Icons.add),
       ),
     );
