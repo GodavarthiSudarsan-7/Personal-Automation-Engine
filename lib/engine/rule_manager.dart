6037c68f7add1f2b1f4a7de6c34c0966b7fd5cf3
Sudarshan <godlsudarsan@gmail.com>
2026-03-08 17:18:50 +0530

    engine folder having the logic behind the rule for managing

--- lib/engine/rule_manager.dart
+++ lib/engine/rule_manager.dart
@@ -6,21 +6,35 @@ class RuleManager {
   static List<Rule> rules = [];
 
   static void loadRules() {
-
     rules = RuleStorage.loadRules();
-
+    print("Loaded rules: ${rules.length}");
   }
 
   static void addRule(Rule rule) {
-
     rules.add(rule);
     RuleStorage.saveRule(rule);
-
   }
 
   static List<Rule> getRules() {
-
     return rules;
+  }
+
+  static void deleteRule(String id) {
+    rules.removeWhere((rule) => rule.id == id);
+    RuleStorage.deleteRule(id);
+  }
+
+  static void toggleRule(String id) {
+
+    for (var rule in rules) {
+
+      if (rule.id == id) {
+        rule.enabled = !rule.enabled;
+        RuleStorage.saveRule(rule);
+        break;
+      }
+
+    }
 
   }
 
